0360c399b47b654cb167c6a1319fcc1100d44eaf
akera02 <elcosto02@gmail.com>
2021-10-28 23:13:55 +0200

    i made some edits :/

---
+++ project2/src/project2/Learn.java
@@ -0,0 +1,18 @@
+package project2;
+import java.util.ArrayList;
+
+public class Learn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> car = new ArrayList<String>();
+		car.add("toyota");
+		car.add("dacia");
+		car.add("ferrari");
+		car.add("porche");
+		car.add("lambergeini");
+		car.add("mercedis");
+		System.out.println(car.size());
+	}
+
+}
---
+++ project2/src/project2/test2222222222.java
@@ -0,0 +1,5 @@
+package project2;
+
+public class test2222222222 {
+ 
+}
